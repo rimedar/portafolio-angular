b2fbc2d4e77fbf3788e76673125e1465cbc41ac3
Ricardo Ramirez <rimedar@hotmail.com>
2019-11-23 23:54:55 -0500

    Menu de navegacion listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  { path: 'home', component: PortafolioComponent},
+  { path: 'item', component: ItemComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, { useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
